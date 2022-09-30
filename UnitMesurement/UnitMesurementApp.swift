7f5851470dfd0692a8013f19927c9f0777842c1d
ribotinho <ribotinho@gmail.com>
2022-09-30 17:14:29 +0200

    Initial Commit

---
+++ UnitMesurement/UnitMesurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitMesurementApp.swift
+//  UnitMesurement
+//
+//  Created by Pau Ribot Pujolras on 30/9/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitMesurementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
